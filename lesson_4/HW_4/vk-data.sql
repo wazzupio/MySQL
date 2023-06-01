75e2982e091ea42fc4e30b0fc0e4af110d4a676a
Wazzapio <myster1on.toks@gmail.com>
2023-06-01 16:52:36 +0300

    add hw_4

---
+++ lesson_4/HW_4/vk-data.sql
@@ -0,0 +1,156 @@
+--
+-- Table structure for table `communities`
+--
+
+CREATE TABLE `communities` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(145) NOT NULL,
+  `description` varchar(245) DEFAULT NULL,
+  `admin_id` bigint unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_communities_users_admin_idx` (`admin_id`),
+  CONSTRAINT `fk_communities_users` FOREIGN KEY (`admin_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB;
+
+--
+-- Table structure for table `communities_users`
+--
+
+CREATE TABLE `communities_users` (
+  `community_id` bigint unsigned NOT NULL,
+  `user_id` bigint unsigned NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`community_id`,`user_id`),
+  KEY `communities_users_comm_idx` (`community_id`),
+  KEY `communities_users_users_idx` (`user_id`),
+  CONSTRAINT `fk_communities_users_comm` FOREIGN KEY (`community_id`) REFERENCES `communities` (`id`),
+  CONSTRAINT `fk_communities_users_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB;
+
+--
+-- Table structure for table `friend_requests`
+--
+
+CREATE TABLE `friend_requests` (
+  `from_user_id` bigint unsigned NOT NULL,
+  `to_user_id` bigint unsigned NOT NULL,
+  `accepted` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`from_user_id`,`to_user_id`),
+  KEY `fk_friend_requests_from_user_idx` (`from_user_id`),
+  KEY `fk_friend_requests_to_user_idx` (`to_user_id`),
+  CONSTRAINT `fk_friend_requests_users_1` FOREIGN KEY (`from_user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `fk_friend_requests_users_2` FOREIGN KEY (`to_user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB;
+
+--
+-- Table structure for table `media`
+--
+
+CREATE TABLE `media` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` bigint unsigned NOT NULL,
+  `media_types_id` int unsigned NOT NULL,
+  `file_name` varchar(245) DEFAULT NULL COMMENT '/files/folder/img.png',
+  `file_size` bigint DEFAULT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `fk_media_users_idx` (`user_id`),
+  KEY `fk_media_media_types` (`media_types_id`),
+  CONSTRAINT `fk_media_media_types` FOREIGN KEY (`media_types_id`) REFERENCES `media_types` (`id`),
+  CONSTRAINT `fk_media_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB;
+
+--
+-- Table structure for table `media_types`
+--
+
+CREATE TABLE `media_types` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB;
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `from_user_id` bigint unsigned NOT NULL,
+  `to_user_id` bigint unsigned NOT NULL,
+  `txt` text NOT NULL,
+  `is_delivered` tinyint(1) DEFAULT '0',
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_at` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `fk_messages_from_user_idx` (`from_user_id`),
+  KEY `fk_messages_to_user_idx` (`to_user_id`),
+  CONSTRAINT `fk_messages_users_1` FOREIGN KEY (`from_user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `fk_messages_users_2` FOREIGN KEY (`to_user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB;
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` bigint unsigned NOT NULL,
+  `txt` text NOT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `user_idx` (`user_id`),
+  CONSTRAINT `user_posts_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB;
+
+--
+-- Table structure for table `posts_likes`
+--
+
+CREATE TABLE `posts_likes` (
+  `post_id` bigint unsigned NOT NULL,
+  `user_id` bigint unsigned NOT NULL,
+  `like_type` tinyint(1) DEFAULT '1',
+  PRIMARY KEY (`post_id`,`user_id`),
+  KEY `post_idx` (`post_id`),
+  KEY `user_idx` (`user_id`),
+  CONSTRAINT `posts_likes_fk` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`),
+  CONSTRAINT `users_likes_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB;
+
+--
+-- Table structure for table `profiles`
+--
+
+CREATE TABLE `profiles` (
+  `user_id` bigint unsigned NOT NULL,
+  `gender` enum('f','m','x') NOT NULL,
+  `birthday` date NOT NULL,
+  `photo_id` bigint unsigned DEFAULT NULL,
+  `user_status` varchar(30) DEFAULT NULL,
+  `city` varchar(130) DEFAULT NULL,
+  `country` varchar(130) DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `photo_id` (`photo_id`),
+  CONSTRAINT `fk_profiles_media` FOREIGN KEY (`photo_id`) REFERENCES `media` (`id`),
+  CONSTRAINT `fk_profiles_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB;
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(145) NOT NULL,
+  `last_name` varchar(145) NOT NULL,
+  `email` varchar(145) NOT NULL,
+  `phone` char(11) NOT NULL,
+  `password_hash` char(65) DEFAULT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email_idx` (`email`),
+  UNIQUE KEY `phone_idx` (`phone`)
+) ENGINE=InnoDB;
